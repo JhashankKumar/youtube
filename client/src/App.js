1305f53e70d32f3282aacaaee61b2591a69f5dd4
THIRUKKULURU JHASHANK KUMAR <122610590+JhashankKumar@users.noreply.github.com>
2024-06-11 11:14:16 +0530

    Update App.js
    some edited mistakes appeared

--- client/src/App.js
+++ client/src/App.js
@@ -16,8 +16,8 @@ import axios from "axios";
 //         onChange={(e) => setQuery(e.target.value.toLowerCase())}
 //       />
 //       <ul className="list">
-//         {Users.filter((asd) =>
-//           asd.first_name.toLowerCase().includes(query)
+//         {Users.filter((user) =>
+//           user.first_name.toLowerCase().includes(query)
 //         ).map((user) => (
 //           <li className="listItem" key={user.id}>
 //             {user.first_name}
